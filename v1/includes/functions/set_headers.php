8c5b86d4731abb744fb85c316e81653f2db44aed
Hery RAKOTONARIVO <hery@odity.fr>
2016-07-13 16:47:26 +0300

    test push after deleting .idea

--- v1/includes/functions/set_headers.php
+++ v1/includes/functions/set_headers.php
@@ -2,6 +2,7 @@
 /**
  * Add header params and catch CORS : OPTIONS REQUEST
  */
+
 if (isset($_SERVER['HTTP_ORIGIN'])) {
     header("Access-Control-Allow-Origin: {$_SERVER['HTTP_ORIGIN']}");
     header('Access-Control-Allow-Credentials: true');
